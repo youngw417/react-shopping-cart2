d11c7a8c08afaa7a85e7c6b52f47609e36857373
Young Won <youngwon@Youngs-MacBook-Pro.local>
2020-10-16 00:39:29 -0700

    cart-component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,45 @@ import React from 'react';
 import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
-
+import Cart from './components/Cart';
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: '',
       sort: '',
     };
   }
+
+  addToCart = (product) => {
+    const cartItems = [...this.state.cartItems];
+
+    let alreadyInCart = false;
+
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+
+    this.setState({ cartItems });
+  };
+
+  removeFromCart = (product) => {
+    let cartItems = [...this.state.cartItems];
+
+    cartItems = cartItems.filter((item) => item._id !== product._id);
+
+    this.setState({ ...product, cartItems });
+  };
+
   sortProducts = (event) => {
     const sort = event.target.value;
     this.setState({
@@ -31,7 +60,7 @@ class App extends React.Component {
   };
 
   filterProducts = (e) => {
-    if (e.target.value === ' ') {
+    if (e.target.value === '') {
       this.setState({
         size: e.target.value,
         products: data.products,
@@ -62,9 +91,17 @@ class App extends React.Component {
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts}
               />
-              <Products products={this.state.products} />
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
             </div>
-            <div className="sidebar">Cart items</div>
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
+            </div>
           </div>
         </main>
         <footer>All right reverved.</footer>
