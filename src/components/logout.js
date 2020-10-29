58d61e23a107e745cb65678ea03469bedf68dd1f
Young Won <youngwon@Youngs-MBP.attlocal.net>
2020-10-29 14:06:28 -0700

    logout added

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../actions/userActions';
+import { Link } from 'react-router-dom';
 
 const logOut = (props) => {
-  return <div>{props.logout()}</div>;
+  const loggingout = () => {
+    props.logout();
+    localStorage.removeItem('token');
+  };
+
+  return (
+    <div>
+      <Link to="/logout" onClick={() => loggingout()}>
+        Logout
+      </Link>{' '}
+    </div>
+  );
 };
 
 export default connect(null, { logout })(logOut);
